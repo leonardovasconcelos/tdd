61c697794dc37ceebd3ab11153f83be5247eaf5e
Leo <lpiovasconcelos@gmail.com>
2022-08-21 19:09:44 -0300

    ensure HttpAdapter calls post with correct Headers

--- test/data/infra/http/http_adapter_test.dart
+++ test/data/infra/http/http_adapter_test.dart
@@ -10,7 +10,11 @@ class HttpAdapter {
   HttpAdapter(this.client);
 
   Future<void> request({@required String url, @required String method}) async {
-    await client.post(Uri.parse(url));
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(Uri.parse(url), headers: headers);
   }
 }
 
@@ -25,7 +29,10 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(Uri.parse(url)));
+      verify(client.post(Uri.parse(url), headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
     });
   });
 }
