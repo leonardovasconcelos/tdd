2d1265720c815bc0909740b7fdebf359ab322830
Leo <lpiovasconcelos@gmail.com>
2022-08-22 03:15:39 -0300

    ensure HttpAdapter throws ForbiddenError on 403

--- test/data/infra/http/http_adapter_test.dart
+++ test/data/infra/http/http_adapter_test.dart
@@ -105,6 +105,14 @@ void main() {
       expect(future, throwsA(HttpError.unauthorized));
     });
 
+    test('Should return ForbiddenError if post return 403', () async {
+      mockResponse(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
     test('Should return ServerError if post return 500', () async {
       mockResponse(500);
 
