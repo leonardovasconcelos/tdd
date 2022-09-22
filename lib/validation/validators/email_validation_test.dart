88f0c46a8daca380dc0be48700477a901b1bca0f
Leo <lpiovasconcelos@gmail.com>
2022-09-22 04:39:31 -0300

    ensure EmailValidation returns error if email is invalid

--- lib/validation/validators/email_validation_test.dart
+++ lib/validation/validators/email_validation_test.dart
@@ -9,7 +9,10 @@ class EmailValidation implements FieldValidation {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -30,5 +33,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('lpiovasconcelos@gmail.com'), null);
+  });
+
+  test('Should return null if email is invalid', () {
+    expect(sut.validate('lpiovasconcelos'), 'Campo inválido');
   });
 }
